6b66b15f4c66f55ac8e3b2fb74a50fac063bc6d3
Mohammed Abu-Al Mahadi <47682811+Mahadi001@users.noreply.github.com>
2021-05-24 18:46:03 +0600

    initial files

---
+++ assets/functions.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    dragft();
+    firstAnim();
+    fdelay();
+    secondAnim();
+    secdelay();
+    thirdAnim();
+})
+function firstAnim(){
+    $('#text1').addClass('animate_fadeIn');
+}
+function fdelay(){
+    setTimeout(function (){
+        $('#fbg').fadeOut();
+    },1200)
+}
+function secondAnim(){
+    setTimeout(function () {
+        $('#pack').addClass('animate_bigToSmall');
+        $('#leaf').addClass('animate_fadeIn');
+        $('#whitebottom').addClass('animate_downToUp');
+        $('#logo').addClass('animate_leftToRight');
+    },1500);
+}
+function secdelay(){
+    setTimeout(function (){
+        $('#secbg').fadeOut();
+    },3000)
+}
+function thirdAnim(){
+    setTimeout(function () {
+        $('#thirdbg').addClass('vis');
+        $('#pack2').addClass('animate_bigtoSmallCenterToLeft');
+        $('#text2').addClass('animate_textLeftToRight');
+        $('#whitebottom2').addClass('animate_downToUp');
+        $('#logo2').addClass('animate_leftToRight');
+    },3000);
+}
+
+document.getElementById('cta').ondragstart = function() { return false; };
+document.getElementById('fbg').ondragstart = function() { return false; };
+document.getElementById('text1').ondragstart = function() { return false; };
+
+
